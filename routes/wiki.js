adfcdbd8a28ae4834d5037315e626bbee8d26ed1
John Arroyo <johnarroyo45@gmail.com>
2022-03-29 21:44:03 -0500

    finished showing a wiki page section, able to display single and all pages

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const { addPage } = require("../views");
+const { addPage, wikiPage, main } = require("../views");
 const { Page } = require("../models");
 
 // For RESTful routing, I'll do GET, POST, PUT, DELETE routes in that order
 
-router.get("/", (req, res, next) => {
-  res.send("<h1>/wiki get route</h1>");
+router.get("/", async (req, res, next) => {
+  const pages = await Page.findAll();
+  res.send(main(pages));
 });
 
 router.get("/add", (req, res, next) => {
   res.send(addPage());
+});
+
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const title = req.params.slug;
+    console.log(title);
+    const page = await Page.findAll({
+      where: {
+        slug: title,
+      },
+    });
+    res.send(wikiPage(page[0]));
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
@@ -19,7 +35,7 @@ router.post("/", async (req, res, next) => {
       title: req.body.title,
       content: req.body.content,
     });
-    res.redirect("/");
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
--- views/main.js
+++ views/main.js
@@ -1,16 +1,20 @@
 const html = require("html-template-tag");
 const layout = require("./layout");
 
-module.exports = (pages) => layout(html`
-  <h3>Pages</h3>
-  <hr>
-  <form method="GET" action="/wiki/search">
-    <input type="text" name="search" />
-    <button type="submit">Search</button>
-  </form>
-  <hr>
-  <ul class="list-unstyled">
-    <ul>
-      <!-- PLACEHOLDER LIST OF PAGES -->
-    </ul>
-  </ul>`);
+module.exports = (pages) =>
+  layout(html` <h3>Pages</h3>
+    <hr />
+    <form method="GET" action="/wiki/search">
+      <input type="text" name="search" />
+      <button type="submit">Search</button>
+    </form>
+    <hr />
+    <ul class="list-unstyled">
+      <ul>
+        <li>
+          ${pages.map((page) => {
+            return `<a href=/wiki/${page.slug}>${page.title}</a>`;
+          })}
+        </li>
+      </ul>
+    </ul>`);
